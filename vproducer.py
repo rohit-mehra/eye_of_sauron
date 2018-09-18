2d7439000b308bb88a684a0127d1d4ba07c55b59
rohit mehra <contactrohitmehra@gmail.com>
2018-09-18 17:31:45 +0000

    added streaming from s3

--- vproducer.py
+++ vproducer.py
@@ -2,6 +2,29 @@ import time
 import cv2
 from kafka import SimpleProducer, KafkaClient
 import os
+import boto3
+import sys
+from io import BytesIO
+
+aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
+resource = boto3.resource('s3') # high-level object-oriented API
+my_bucket = resource.Bucket('camera-data-eye') # s3 bucket name. 
+
+# get object meta data in the directory
+files = list(my_bucket.objects.filter(Prefix='cam1/videos/'))
+
+print(files)
+
+# choose and object and get its content
+#choice = 0
+#vid_obj = files[choice].get()['Body']
+#vid_key = files[choice].key
+#print("Reading: {}".format(vid_key))
+#vid_stream = BytesIO(vid_obj.read())
+
+# sys.exit(0)
+
 #  connect to Kafka
 kafka = KafkaClient('localhost:9092')
 producer = SimpleProducer(kafka)
@@ -10,6 +33,9 @@ producer = SimpleProducer(kafka)
 topic = 'frames'
 
 video_path = "/home/ubuntu/eye_of_sauron/data/cam1/videos/cam1_2_fps.mp4" 
+
+# serving from s3 bucket via cloudFront: url to the object
+cfront_url = "http://dqjqvfw96ipmn.cloudfront.net/cam0_2_fps.mp4"
 
 print(os.listdir("/home/ubuntu/eye_of_sauron/data/cam1/videos/"))
 
@@ -40,4 +66,5 @@ def video_emitter(video):
 
 if __name__ == '__main__':
     
-    video_emitter(video_path)
+    # video_emitter(video_path)
+    video_emitter(cfront_url)
