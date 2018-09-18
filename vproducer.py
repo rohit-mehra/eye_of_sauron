b2a8b9de2473c6d7069281aca95da4167a4a710c
rohit mehra <contactrohitmehra@gmail.com>
2018-09-18 15:36:50 +0000

    fixed video path

--- vproducer.py
+++ vproducer.py
@@ -1,7 +1,7 @@
 import time
 import cv2
 from kafka import SimpleProducer, KafkaClient
-
+import os
 #  connect to Kafka
 kafka = KafkaClient('localhost:9092')
 producer = SimpleProducer(kafka)
@@ -9,19 +9,22 @@ producer = SimpleProducer(kafka)
 # Assign a topic
 topic = 'frames'
 
-video_path = "/home/ubuntu/video_stream_kafka/data/cam1/cam1_5_fps.mp4" 
+video_path = "/home/ubuntu/eye_of_sauron/data/cam1/videos/cam1_2_fps.mp4" 
+
+print(os.listdir("/home/ubuntu/eye_of_sauron/data/cam1/videos/"))
 
 def video_emitter(video):
     # Open the video
     video = cv2.VideoCapture(video)
     print('Emitting.....')
-
+    i = 0
     # read the file
     while (video.isOpened):
         # read the image in each frame
         success, image = video.read()
         # check if the file has read to the end
         if not success:
+            print("BREAK AT FRAME: {}".format(i))
             break
         # convert the image png
         ret, jpeg = cv2.imencode('.png', image)
@@ -29,6 +32,7 @@ def video_emitter(video):
         producer.send_messages(topic, jpeg.tobytes())
         # To reduce CPU usage create sleep time of 0.2sec  
         time.sleep(0.1)
+        i += 1
     
     # clear the capture
     video.release()
